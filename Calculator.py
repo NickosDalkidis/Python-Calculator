45ae1384b3af93715a3376a1aceb8ad96f8f3a10
TheBlackReaper23 <90420049+TheBlackReaper23@users.noreply.github.com>
2022-05-30 23:35:55 +0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+from ast import If
+
+
+num1=input("Please Enter a number")
+num2=input("Please Enter Another Number")
+result= float(num1)+float(num2)
+result2=float(num1)*float(num2)
+print("Adittion")
+print(result)
+print("Multiplication")
+print (result2)
