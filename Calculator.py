ac9acb547a4670627d66e6388a0c7b34e674a5d2
NickosDalkidis <90420049+NickosDalkidis@users.noreply.github.com>
2022-06-11 13:10:23 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,23 @@
 from ast import If
 
-
 num1=input("Please Enter a number")
 num2=input("Please Enter Another Number")
 result= float(num1)+float(num2)
 result2=float(num1)*float(num2)
+result3=float(num1)-float(num2)
 base=float(input("Enter the base number"))
 exponent=float(input("Enter the exponent"))
-result3=base**exponent
+result4=base**exponent
+num3=float(input("Please enter the number you want to be divised"))
+num4=float(input("Please enter the number want to divise the previous number with"))
+result5=num3//num4
 print("Addition")
 print(result)
 print("Multiplication")
 print (result2)
+print("Subtraction")
+print(result3)
 print ("raise to power")
-print (result3)
+print (result4)
+print("Division")
+print (result5)
